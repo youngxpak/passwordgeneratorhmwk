ecad971734e1c3ead4ce62faa171324b0fd399c2
Young Pak <youngxpak@gmail.com>
2019-12-04 14:43:20 -0500

    added functions

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 //Generate random Password
 
-var char = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
-var num = "0 1 2 3 4 5 6 7 8 9";
-var symbol = "! @ # $ % ^ & * + ?";
+function genPassword(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
 
-function Password()
+console.log(genPassword(5));
+
+<button onclick="myFunction()">Generate Password</button>
+
