6e415f91d144582b4ea8dd193a7b902008e92fce
Young Pak <youngxpak@gmail.com>
2019-12-04 23:40:13 -0500

    added some text to read me

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # passwordgeneratorhmwk
+
+## Title
+Password Generator
+
+## Description
+Generates a secure multi faceted password including Alphanumeric and case sensative characters
+
+
+## Usage
+Solves the issue of most modern day password requirements. Allows the computer to create everything for you easily.
+
+## Project status
+Unfinsihed Ran out of time. 
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 var num = "0123456789";
 var symbol = "! @ # $ % ^ & * + ?";
 
-// prompt for options
+// prompt for length
 function passwordLengthPrompt(){
     var length = prompt ("how long password");
     if(isNaNlength || !length)
@@ -12,14 +12,19 @@ function passwordLengthPrompt(){
     }
     return length;
 
-//prompt for options
 function generatePassword(){
     var passwordLength = passwordLengthPrompt();
     if (passwordLength <= 7 || passwordLength > 128){
         alert("Too Short!") 
     }
        
-    
+    var includeUpperCase = confirm ("Do you want Uppercase Letters?");
+    var includeLowerCase = confirm ("Do you want Lowercase Letters?");
+    var includeNumber = confirm ("Do you want a number?");
+    var includwSymbol = confirm ("Do you want a symbol?");
+    if(!includeUpperCase && !includeLowerCase && !includeNumber && !includwSymbol) {
+        alert("You need one of each!")
+    }
 }
 
 function getRandomUpper()
